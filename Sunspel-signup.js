4ea6e402ee462aaca512e2637fe8aea79b236ddb
Venask4 <mgcoloe@gmail.com>
2017-05-13 16:55:13 -0400

    Implement client comments 5/12/17

--- Sunspel-signup.js
+++ Sunspel-signup.js
@@ -23,8 +23,8 @@ var exp = (function($) {
 	exp.vars = {
 		telephoneMicrocopy: '<span class="AWA-microcopy">(For delivery updates)</span>',
 		passwordMicrocopy: '<span class="AWA-microcopy">Please enter a password for your Sunspel account</span><br>',
-		enterAddress: '<div id="enterAddress"><a href="javascript:void(0);">Enter Address Manually</a><br><br></div>',
-		postCodeMicrocopy: '<span class="AWA-microcopy">Please use your post code to find your address</span><br>'
+		enterAddress: '<div class="AWA-enterAddress"><a href="javascript:void(0);">Enter Address Manually</a><br><br></div>',
+		postCodeMicrocopy: '<span class="AWA-microcopy AWA-microcopy-grey">Please use your post code to find your address</span>'
 	};
 
 	// Styles
@@ -40,6 +40,19 @@ var exp = (function($) {
 			letter-spacing: .025em;\
 			text-transform: none;\
 		}\
+		.AWA-microcopy-grey {\
+			background: #f2f2f2;\
+			padding: 3.75px 7.5px;\
+			border-left: 3.75px solid #3c3;\
+			display: block;\
+			width: 480px;\
+			margin-bottom: 7px;\
+		}\
+		@media only screen and (max-width: 599px) {\
+			.AWA-microcopy-grey {\
+				width: 100%;\
+			}\
+		}\
 	';
 
 	// Init function
@@ -49,17 +62,19 @@ var exp = (function($) {
 		$('head').append('<style>' + exp.css + '</style>');
 
 		//Hide Middle Initial and Date of Birth forms
-		$('#billing-new-address-form .name-middlename').hide();
+		$('.name-middlename').hide();
 		$('.field .input-combined').siblings('label').hide();
 		$('.field .input-combined').hide();
 
 		// Move Post Code Div up to beneath E-mail Div
 		var $postCodeField = $('.field label:contains("Post Code")').first().parent();
 		var $emailField = $('#billing-new-address-form .field label:contains("Email")').first().parent();
-		$emailField.after($postCodeField);
+		if ($emailField.length) {
+			$emailField.after($postCodeField);
+		}
 
 		// Add microcopy to Telephone label
-		var $telephoneField = $('#billing-new-address-form .field label:contains("Telephone")').first();
+		var $telephoneField = $('.field label:contains("Telephone")');
 		$telephoneField.append(exp.vars.telephoneMicrocopy);
 
 		// Add microcopy to Password label
@@ -67,19 +82,20 @@ var exp = (function($) {
 		$passwordField.prepend(exp.vars.passwordMicrocopy);
 
 		// Add microcopy to Post Code label
-		$('.field label:contains("Post Code")').first().prepend(exp.vars.postCodeMicrocopy);
+		$('.field label:contains("Post Code")').prepend(exp.vars.postCodeMicrocopy);
 
 		// Hide address fields
-		$('#billing-new-address-form .field label:contains("Company")').first().parent().hide();
-		$('#billing-new-address-form .field label:contains("Street Address")').parent().hide();
-		$('#billing-new-address-form .field label:contains("Town")').first().parent().hide();
-		$('#billing-new-address-form .field label:contains("Count")').parent().hide();
-		$postCodeField.after(exp.vars.enterAddress);
-		$('#enterAddress').on('click', function() {
-			$('#billing-new-address-form .field label:contains("Company")').first().parent().show();
-			$('#billing-new-address-form .field label:contains("Street Address")').parent().show();
-			$('#billing-new-address-form .field label:contains("Town")').first().parent().show();
-			$('#billing-new-address-form .field label:contains("Count")').parent().show();
+		$('.field label:contains("Company")').parent().hide();
+		$('.field label:contains("Street Address")').parent().hide();
+		$('.field label:contains("Town")').parent().hide();
+		$('.field label:contains("Count")').parent().hide();
+		$('.field label:contains("Post Code")').parent().after(exp.vars.enterAddress);
+		$('.AWA-enterAddress').on('click', function() {
+			$('.field label:contains("Company")').parent().show();
+			$('.field label:contains("Street Address")').parent().show();
+			$('.field label:contains("Town")').parent().show();
+			$('.field label:contains("Count")').parent().show();
+			$('.AWA-microcopy-grey').hide();
 			$(this).remove();
 		})
 	};
